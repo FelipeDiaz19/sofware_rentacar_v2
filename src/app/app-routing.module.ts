d957094d20c215e3bd090b4f694a221a29f65898
diego rios rojas <d.riosrojas007@gmail.com>
2020-12-17 16:06:49 -0300

    contruccion de estructura basica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { TarifasComponent } from './views/tarifas/tarifas.component';
+import { ItemComponent } from './views/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'tarifas', component: TarifasComponent },
+  { path: 'item', component: ItemComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy', useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
+//rutas
 import { AppRoutingModule } from './app-routing.module';
+
+
+
+// servicios
+
+
+
+// components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { SidebarsComponent } from './template/sidebars/sidebars.component';
+import { HomeComponent } from './views/home/home.component';
+import { TarifasComponent } from './views/tarifas/tarifas.component';
+import { ItemComponent } from './views/item/item.component';
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarsComponent,
+    TarifasComponent,
+    ItemComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,

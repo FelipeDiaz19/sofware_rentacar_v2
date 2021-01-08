a1e8dc866769a8fda0626f18490bed67a8ea7882
diego rios rojas <d.riosrojas007@gmail.com>
2021-01-08 16:07:05 -0300

    modificaciobn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://www.imlchile.cl:3010/rentacar/",
+  apiUrl: "https://www.imlchile.cl:3011/rentacar/",
   indexUrl: "https://www.imlchile.cl/rentacar/"
 };

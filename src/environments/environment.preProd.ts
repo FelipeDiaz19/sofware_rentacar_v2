cf60796cc21e8fef28626863510ccafbde353b66
diego rios rojas <d.riosrojas007@gmail.com>
2021-02-18 18:09:48 -0300

    creacion de entorno de prueba

---
+++ src/environments/environment.preProd.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+    apiUrl: "https://www.imlchile.cl:3011/rentacar/",
+    indexUrl: "https://www.imlchile.cl/rentacar_test/"
+};
+
+/*
+ apiUrl: "https://www.imlchile.cl:3011/rentacar/",
+  indexUrl: "https://www.imlchile.cl/rentacar_test/" */
+
+
+/*
+ apiUrl: "https://www.imlchile.cl:3010/rentacar/",
+ indexUrl: "https://www.imlchile.cl/rentacar/"
+*/
